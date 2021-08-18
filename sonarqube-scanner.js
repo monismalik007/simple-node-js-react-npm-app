e927e6edcd40fb456607df39d3f1626603f0587d
Monis Amin <40907691+monismalik007@users.noreply.github.com>
2021-08-18 12:31:07 +0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,7 +2,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
     {
         serverUrl: "http://localhost:9000",
-        token: "46a57961104ec570e23ca1f8a696ed9ca6d5fe62",
+        token: "707e7afe449e9958104e7f8f1e5b51ccf899810b",
         options: {
             "sonar.sources": "./src"
         },
